cc62906805aa918132f28f7b4860ec3be0b61006
Terranie Sims <terranie67@gmail.com>
2020-09-13 21:43:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  min_value = 100 
   name_hash.each do |key, value|
-    
-    
+    if value < min_value 
+      min_value = value 
+      value = key 
   end 
-
 end
