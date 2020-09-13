dff7b4c5eca5630f2040b51aab571b6d49aca803
Terranie Sims <terranie67@gmail.com>
2020-09-13 21:53:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,5 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-require 'pry'
 
 def key_for_min_value(name_hash)
   min_value = 100 
@@ -8,7 +7,6 @@ def key_for_min_value(name_hash)
     if value < min_value 
       min_value = value 
       value = key 
-      binding.pry 
     end 
   end 
 end
