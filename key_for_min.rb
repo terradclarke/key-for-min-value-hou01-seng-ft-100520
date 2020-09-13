ba896913d1d45134e847652dcf3a7bda0c01bf63
Terranie Sims <terranie67@gmail.com>
2020-09-13 22:46:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   min_value = nil  
   lowest_value = Float::INFINITY
-  name_hash.ech do |key, value|
+  name_hash.each do |key, value|
     if value < min_value
       lowest_key = value
       lowest_key = key  
