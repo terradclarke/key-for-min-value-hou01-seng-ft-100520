434f74e4bd57874a94a9136d193702a749428dea
Terranie Sims <terranie67@gmail.com>
2020-09-13 21:53:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@ require 'pry'
 
 def key_for_min_value(name_hash)
   min_value = 100 
-  name_hash.each do |key, value|
+  name_hash.collect do |key, value|
     if value < min_value 
       min_value = value 
       value = key 
